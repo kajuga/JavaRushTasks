9e75766970e25fc75677262af373737d1905405c
kajuga <kajuga@mail.ru>
2019-06-30 22:06:15 +0300

    30/06/19

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1011/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task10.task1011;
+
+/* 
+Большая зарплата
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "Я не хочу изучать Java, я хочу большую зарплату";
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.substring(i));
+        }
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1012/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task10.task1012;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 
+Количество букв
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // Алфавит
+        List<Character> alphabet = Arrays.asList(
+                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж',
+                'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+                'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+                'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+
+        // Ввод строк
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 2; i++) {
+            String s = reader.readLine();
+            list.add(s.toLowerCase());
+        }
+        for (int i = 0; i < alphabet.size(); i++) {
+            System.out.println(alphabet.get(i) + " " + counter(alphabet.get(i), list));
+        }
+    }
+
+    public static int counter(char chr, List<String> str) {
+        int result = 0;
+        for (int i = 0; i < str.size(); i++) {
+            for (int j = 0; j < str.get(i).length(); j++) {
+                if (str.get(i).charAt(j) == chr) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
