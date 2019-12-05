e6b82beb707b380e6dca6e91bc0048ee099eb948
kajuga <kajuga@mail.ru>
2019-12-05 22:55:29 +0300

    05/12/19

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1618/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task16.task1618;
+
+/* 
+Снова interrupt
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        //Add your code here - добавь код тут
+        TestThread thread = new TestThread();
+        thread.start();
+        Thread.sleep(3000);
+        thread.interrupt();
+
+    }
+
+    //Add your code below - добавь код ниже
+    public static class TestThread extends Thread {
+
+        public void run(){
+            System.out.println("Я работаю  - юхуууууу!");
+
+        }
+
+    }
+}
