91a2e749a4d9e1e639cb680ae9daf362c01defda
kajuga <kajuga@mail.ru>
2020-02-05 14:58:33 +0300

    05/02/20

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        try {
+            int first = string.indexOf("\t") + 1;
+            return string.substring(first, string.indexOf("\t", first));
+        } catch (Exception e) {
+            throw new TooShortStringException();
+        }
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+    }
+}
