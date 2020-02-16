9a085007eafe5e4b828581f7d4fa9a1a1889321b
kajuga <kajuga@mail.ru>
2020-02-16 22:09:19 +0300

    17/02/20

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3205/CustomInvocationHandler.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task32.task3205;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class CustomInvocationHandler implements InvocationHandler {
+    private SomeInterfaceWithMethods someInterfaceWithMethods;
+
+    public CustomInvocationHandler(SomeInterfaceWithMethods a) {
+        this.someInterfaceWithMethods = a;
+
+    }
+
+    @Override
+    public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+        System.out.println(method.getName() + " in");
+        Object res = method.invoke(someInterfaceWithMethods, objects);
+        System.out.println(method.getName() + " out" );
+        return res;
+
+    }
+}
--- 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
@@ -7,7 +7,6 @@ import java.security.MessageDigest;
 /* 
 Целостность информации
 */
-
 public class Solution {
     public static void main(String... args) throws Exception {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -15,7 +14,6 @@ public class Solution {
         oos.writeObject(new String("test string"));
         oos.flush();
         System.out.println(compareMD5(bos, "5a47d12a2e3f9fecf2d9ba1fd98152eb")); //true
-
     }
 
     public static boolean compareMD5(ByteArrayOutputStream byteArrayOutputStream, String md5) throws Exception {
@@ -26,7 +24,6 @@ public class Solution {
         for(byte e : digest) {
             sb.append(String.format("%02x", e & 0xff));
         }
-
         System.out.println(sb);
         return sb.toString().equals(md5);
     }
